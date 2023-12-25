ee90bded053073c34873e7903da2f802eeed5aab
MyCode07 <erem19992015@mail.ru>
2023-12-25 15:53:20 +0300

    hero page all sections is done

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,18 @@
 // import { accorden } from "./static/accordeon.js";
 // import { maskInputs } from "./static/inputmask.js";
 // import { runTicker } from "./static/ticker.js";
-// import { replaceDomElements } from "./static/replace.js";
+import { replaceDomElements } from "./static/replace.js";
 // import { toTop } from "./static/to-top.js";
 // import { stickyHeader } from "./parts/header.js";
 // import './utils/smoothscrol.js';
 
-import { animateStaggerAction } from "./parts/animations.js";
+import { animateAction, animateStaggerAction } from "./parts/animations.js";
 
 animateStaggerAction();
+animateAction();
 // stickyHeader()
 // toTop();
-// replaceDomElements();
+replaceDomElements();
 // runTicker()
 // accorden();
 // maskInputs('+7 999 999 999 999', '.phone')
--- src/js/parts/animations.js
+++ src/js/parts/animations.js
@@ -1,4 +1,56 @@
+
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
+gsap.registerPlugin(ScrollTrigger)
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            animate(entry.target);
+            animateLogo(entry.target)
+        }
+    })
+});
+
+
+function animate(elem) {
+    const delay = elem.dataset.delay ? elem.dataset.delay * 1000 : 0
+
+    if (elem.hasAttribute('data-animate-left')) {
+        setTimeout(() => {
+            elem.removeAttribute('data-animate-left');
+        }, delay);
+    }
+    if (elem.hasAttribute('data-animate-right')) {
+        setTimeout(() => {
+            elem.removeAttribute('data-animate-right');
+        }, delay);
+    }
+    if (elem.hasAttribute('data-animate-top')) {
+        setTimeout(() => {
+            elem.removeAttribute('data-animate-top');
+        }, delay);
+    }
+    if (elem.hasAttribute('data-animate-bottom')) {
+        setTimeout(() => {
+            elem.removeAttribute('data-animate-bottom');
+        }, delay);
+    }
+    if (elem.hasAttribute('data-animate-opacity')) {
+        setTimeout(() => {
+            elem.removeAttribute('data-animate-opacity');
+        }, delay);
+    }
+}
+
+const animateElems = document.querySelectorAll('[data-animate]');
+export const animateAction = () => {
+    if (!animateElems.length) return;
+
+    animateElems.forEach(elem => {
+        observer.observe(elem);
+    })
+}
 
 // stagger animations
 const observerStagger = new IntersectionObserver((entries, self) => {
@@ -18,10 +70,10 @@ function animateStagger(elem) {
     if (elem) {
         gsap.to(elem, {
             opacity: 1,
-            duration: 0.5,
+            duration: 0.3,
             x: 0,
             y: 0,
-            ease: 'cubic-bezier(0.16, 1.08, 0.38, 0.98)',
+            ease: 'ease',
             stagger: 0.2
         });
     }
@@ -32,6 +84,23 @@ export const animateStaggerAction = () => {
     if (!animateElemsStagger.length) return
     animateElemsStagger.forEach(elem => {
         observerStagger.observe(elem);
+
     })
 }
 
+
+function animateLogo(logo) {
+    const paths = logo.querySelectorAll('path');
+    if (!paths.length) return;
+
+    gsap.to(paths, {
+        opacity: 1,
+        y: 0,
+        duration: 0.3,
+        stagger: 0.1,
+        ease: 'ease'
+    });
+}
+
+const footerLogo = document.querySelector('.footer__logo svg');
+observer.observe(footerLogo);
