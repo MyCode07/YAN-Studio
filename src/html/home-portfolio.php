26e32c10a4ba70218db1bcdaf0b1981e67f0f77b
MyCode07 <erem19992015@mail.ru>
2024-01-19 21:35:37 +0300

    cahnge portfolio

---
+++ src/html/home-portfolio.php
@@ -0,0 +1,85 @@
+<section class="section portfolio" data-replace-width="992">
+        <div class="section__container _container">
+            <div class="section__body">
+                <div class="section__content" data-replace-new-position="afterend">
+                    <div class="section__top grid grid-2" data-replace-old-position="beforeend" data-animate-stagger
+                        data-animate-bottom>
+                        <h2>Портфолио</h2>
+                        <a href="portfolio.html" class="_link-btn _underline" data-replace-element>
+                            <div>
+                                <span><i>В</i><i>с</i><i>е</i> <i>р</i><i>а</i><i>б</i><i>о</i><i>т</i><i>ы</i></span>
+                                <span><i>В</i><i>с</i><i>е</i> <i>р</i><i>а</i><i>б</i><i>о</i><i>т</i><i>ы</i></span>
+                            </div>
+                            <svg width="12" height="4">
+                                <use xlink:href="img/svg/icons.svg#link-arrow"></use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="section__bottom grid grid-2">
+                        <a href="https://origamitea.ru/" class="protfolio-card _underline" target="_blank"
+                            data-animate-stagger data-animate-bottom>
+                            <div class="protfolio-card__img">
+                                <video playsinline poster="img/portfolio/origami.jpg" muted loop class="_video">
+                                    <source src="files/origami.mp4" type="video/mp4">
+                                </video>
+                            </div>
+                            <div class="protfolio-card__info">
+                                <h3>ORIGAMI</h3>
+                                <p>Свежий и вкусный японский чай ORIGAMI <br> лучшая альтернатива кофе.</p>
+                                <ol class=" grid grid-2">
+                                    <li>Дизайн и разработка</li>
+                                    <li>2022</li>
+                                </ol>
+                            </div>
+                        </a>
+                        <a href="https://busmotors.test-yeremyan.site/" class="protfolio-card _underline"
+                            target="_blank" data-animate-stagger data-animate-bottom>
+                            <div class="protfolio-card__img">
+                                <img loading="lazy" src="img/portfolio/BusMotors.jpg" alt="">
+                            </div>
+                            <div class="protfolio-card__info">
+                                <h3>Bus Motors</h3>
+                                <p>Профессиональное переоборудование <br> Mercedes Benz Sprinter.</p>
+                                <ol class=" grid grid-2">
+                                    <li>Разработка</li>
+                                    <li>2023</li>
+                                </ol>
+                            </div>
+                        </a>
+                        <a href="https://top-proektor.ru/" class="protfolio-card _underline" target="_blank"
+                            data-animate-stagger data-animate-bottom>
+                            <div class="protfolio-card__img">
+                                <video playsinline poster="img/portfolio/TOPProjector.jpg" muted loop class="_video">
+                                    <source src="files/TOPProjector.mp4" type="video/mp4">
+                                </video>
+                            </div>
+                            <div class="protfolio-card__info">
+                                <h3>Топ-проектор</h3>
+                                <p>Продажа и установка гобо проекторов <br> для наружной рекламы.</p>
+                                <ol class=" grid grid-2">
+                                    <li>Разработка</li>
+                                    <li>2023</li>
+                                </ol>
+                            </div>
+                        </a>
+                        <a href="https://dostavka.mphub.ru/" class="protfolio-card _underline" target="_blank"
+                            data-animate-stagger data-animate-bottom>
+                            <div class="protfolio-card__img">
+                                <video playsinline poster="img/portfolio/MPHub.jpg" muted loop class="_video">
+                                    <source src="files/MPHub.mp4" type="video/mp4">
+                                </video>
+                            </div>
+                            <div class="protfolio-card__info">
+                                <h3>MPHUB</h3>
+                                <p>Доставка на маркетплейсы Wildberries, Ozon, <br> МегаМаркет, Яндекс.Маркет.</p>
+                                <ol class=" grid grid-2">
+                                    <li>Разработка</li>
+                                    <li>2023</li>
+                                </ol>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
